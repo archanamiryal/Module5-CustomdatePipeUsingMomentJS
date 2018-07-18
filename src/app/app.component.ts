934f0364c79470022c9c69156e23315985868954
Archana Miryala <archanmiryal>
2018-07-18 00:19:58 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  dat:any;
+  today:any;
+  
+  dateFormat: any = new Date();
+  constructor(){}
+  ngOnInit(): void { }
+  submitForm(formDate)
+    {
+      
+       this.dat=formDate.userDate.value;
+       console.log("data is coming" ,this.dat);
+       this.today=moment(this.dat).fromNow();
+    }
+  
+ 
+  
+  
 }
